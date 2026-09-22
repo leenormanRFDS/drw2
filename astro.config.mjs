775d1fc2771fe2ffafbb5a9e2aba10e326b92368
leenormanRFDS <lee.norman@flyingdoctor.net>
2026-09-22 14:51:54 +0930

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://thebombingofdarwin.com.au',
+  compressHTML: true,
+
+  output: 'server',
+  adapter: vercel(),
+
+  devToolbar: {
+    enabled: false
+  }
+});
